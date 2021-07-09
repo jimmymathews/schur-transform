37a457f6ba7e7ed4086e1a76ed12de2afba49e4f
Jimmy Mathews <mathewj2@mskcc.org>
2021-07-08 23:50:14 -0400

    started expanding api exposure

--- schurtransform/__init__.py
+++ schurtransform/__init__.py
@@ -1,4 +1,8 @@
+import importlib
+import importlib.resources
+
 from .transform import SchurTransform
+from . import lung_data
 
 global_transformer = SchurTransform()
 
@@ -7,3 +11,16 @@ def transform(samples, **kwargs):
     See ``SchurTransform.transform``.
     """
     return global_transformer.transform(samples, **kwargs)
+
+
+with importlib.resources.path(lung_data, 'examples_manifest.txt') as path:
+    example_filenames = open(path).read().split('\n')
+
+with importlib.resources.path(lung_data, example_filenames[0]) as path:
+    txt = open(path).read()
+
+def get_example_filenames():
+    return example_filenames
+
+def get_example_text():
+    return txt
